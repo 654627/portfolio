b08e5cf6086b6f13160036ee1f4fcbf561af6286
654627 <654627@pdsb.net>
2021-03-23 09:41:55 -0400

    j

--- script/main.js
+++ script/main.js
@@ -1,27 +1,27 @@
 function f1() {
-    alert("Script 1 in progress..");
+    alert('coming soon');
 }
 function f2() {
-    alert("Script 2 in progress..");
+    alert('coming soon');
 }
 function f3() {
-    alert("Script 3 in progress..");
+    alert('coming soon');
 }
 function f4() {
-    alert("Script 4 in progress..");
+    alert('coming soon');
 }
 function f5() {
-    alert("Script 5 in progress..");
+    alert('coming soon');
 }
 function f6() {
-    alert("Script 6 in progress..");
+    alert('coming soon');
 }
 function f7() {
-    alert("Script 7 in progress..");
+    alert('coming soon');
 }
 function f8() {
-    alert("Script 8 in progress..");
+    alert('coming soon');
 }
 function f9() {
-    alert("Script 9 in progress..");
+    alert('coming soon');
 }
